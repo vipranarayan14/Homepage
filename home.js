59ec9bc514646d75315e7a09604a57378f8d4024
Prasanna Venkatesh T S <vipranarayan14@gmail.com>
2016-12-07 18:36:16 +0530

    add reload btn

--- home.js
+++ home.js
@@ -1,51 +1,38 @@
 var linkA = document.querySelector("#linkApps");
 var linkE = document.querySelector("#linkExts");
 
+var rssReloaders = document.querySelectorAll('.rssReloader'), i;
+
+for (i = 0; i < rssReloaders.length; ++i) {
+  rssReloaders[i].addEventListener('click', function (e) {
+    Rss.reload(e.target.getAttribute("source"));
+  });
+}
+
 var rssR = document.querySelector("#rssShow");
 
-document.querySelector("#searchInput").addEventListener('keydown', function(e){
-  if(e.keyCode === 13 && this.value !== "") {
+document.querySelector("#searchInput").addEventListener('keydown', function (e) {
+  if (e.keyCode === 13 && this.value !== "") {
     searchGoogle(this.value);
   }
 });
 
-linkA.addEventListener('click', function(){
-chrome.tabs.update({url: "chrome://apps"});
+linkA.addEventListener('click', function () {
+  chrome.tabs.update({ url: "chrome://apps" });
 });
 
-linkE.addEventListener('click', function(){
-chrome.tabs.update({url: "chrome://extensions"});
+linkE.addEventListener('click', function () {
+  chrome.tabs.update({ url: "chrome://extensions" });
 });
 
 function searchGoogle(searchQuery) {
   var serviceCall = 'http://www.google.com/search?q=' + searchQuery;
-  chrome.tabs.update({url: serviceCall});
-}
-
-(function initRss() {
-  var eles = document.querySelectorAll(".rssShow");
-  for (var i=0; i < eles.length; i++) {
-    var urlSrc = eles[i].getAttribute("source");
-    getRss(urlSrc, eles[i]);
-  }
-})();
-
-function getRss(url, ele) {
-  var feed;
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-    loadRss(this.responseText, ele);
-    }
-  };
-  xhttp.open("GET", url, true);
-  xhttp.send();
+  chrome.tabs.update({ url: serviceCall });
 }
 
 
-function loadRss(resp, ele) {
-  //ele.innerHTML = resp;
-}
+Rss.load();
+
 
 console.log("Hello!!");
 
