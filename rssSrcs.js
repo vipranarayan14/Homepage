feaf83f087969633ff4984deb419967c6cf60927
Prasanna Venkatesh T S <vipranarayan14@gmail.com>
2016-12-16 16:50:37 +0530

    Change Rss Sources names to be semantic

--- rssSrcs.js
+++ rssSrcs.js
@@ -1,4 +1,4 @@
 var rssSources = {
-"first" : "http://rss.bloople.net/?url=http%3A%2F%2Fwww.thehindu.com%2Fsci-tech%2Ftechnology%2F%3Fservice%3Drss&showtitle=true&type=html",
-"second" : "http://rss.bloople.net/?url=http%3A%2F%2Fwww.thehindu.com%2Fnews%2Fnational%2F%3Fservice%3Drss&showtitle=true&type=html"
+"THNational" : "http://www.thehindu.com/news/national/?service=rss",
+"THChennai" : "http://www.thehindu.com/news/cities/Chennai/?service=rss"
 }
