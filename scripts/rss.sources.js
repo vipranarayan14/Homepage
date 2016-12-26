f9f39cdbfcb7670ef53d35e4cbfdef902af8499a
Prasanna Venkatesh T S <vipranarayan14@gmail.com>
2016-12-26 11:04:44 +0530

    add new feed url

--- scripts/rss.sources.js
+++ scripts/rss.sources.js
@@ -1,5 +1,6 @@
 var rssSources = {
 "THNational" : "http://www.thehindu.com/news/national/?service=rss",
 "THChennai" : "http://www.thehindu.com/news/cities/Chennai/?service=rss",
-"SDTop" : "https://rss.sciencedaily.com/top.xml"
+"SDTop" : "https://rss.sciencedaily.com/top.xml",
+"SDTos" : "https://rss.sciencedaily.com/top.xml"
 }
