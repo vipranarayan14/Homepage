c06aeb6794bb9c7e467d52dcd0b676d829eab90f
Prasanna Venkatesh T S <vipranarayan14@gmail.com>
2016-12-26 12:35:32 +0530

    add `popsci` feed url

--- scripts/rss.sources.js
+++ scripts/rss.sources.js
@@ -2,5 +2,5 @@ var rssSources = {
 "THNational" : "http://www.thehindu.com/news/national/?service=rss",
 "THChennai" : "http://www.thehindu.com/news/cities/Chennai/?service=rss",
 "SDTop" : "https://rss.sciencedaily.com/top.xml",
-"SDTos" : "https://rss.sciencedaily.com/top.xml"
+"Popsci" : "http://www.popsci.com/syndication/site_default/62"
 }
