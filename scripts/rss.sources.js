277536c7897c819d8bd680bc8fe5f700d5c7f671
Prasanna Venkatesh T S <vipranarayan14@gmail.com>
2017-02-22 22:25:14 +0530

    Substitute Popsci with Sciencemag

--- scripts/rss.sources.js
+++ scripts/rss.sources.js
@@ -2,5 +2,5 @@ var rssSources = {
 "The Hindu - National" : "http://www.thehindu.com/news/national/?service=rss",
 "The Hindu - Chennai" : "http://www.thehindu.com/news/cities/Chennai/?service=rss",
 "Science Daily - Top" : "https://rss.sciencedaily.com/top.xml",
-"Popsci - Science" : "http://www.popsci.com/syndication/site_default/62"
+//"Sciencemag - Science" : "http://science.sciencemag.org/rss/twis.xml"
 }
