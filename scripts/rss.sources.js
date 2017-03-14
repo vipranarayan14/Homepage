7a8c1310f1e7c8a547b30a21402b43c0e5cf2965
Prasanna Venkatesh T S <vipranarayan14@gmail.com>
2017-03-15 01:18:23 +0530

    Move rss-sources to root folder for easy accessibilty of the user

--- scripts/rss.sources.js
+++
@@ -1,6 +0,0 @@
-var rssSources = {
-"The Hindu - National" : "http://www.thehindu.com/news/national/?service=rss",
-"The Hindu - Chennai" : "http://www.thehindu.com/news/cities/Chennai/?service=rss",
-"Science Daily - Top" : "https://rss.sciencedaily.com/top.xml",
-//"Sciencemag - Science" : "http://science.sciencemag.org/rss/twis.xml"
-}
