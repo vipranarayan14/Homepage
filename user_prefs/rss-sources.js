55bf160eb8cb67da291b014aaa1a0994d7a7a5e1
Prasanna Venkatesh <vipranarayan14@gmail.com>
2017-03-15 13:57:29 +0530

    add user_prefs for tracking

--- user_prefs/rss-sources.js
+++ user_prefs/rss-sources.js
@@ -2,6 +2,5 @@
 var rssSources = {
     "The Hindu - National": "http://www.thehindu.com/news/national/?service=rss",
     "The Hindu - Chennai": "http://www.thehindu.com/news/cities/Chennai/?service=rss",
-    "Science Daily - Top": "https://rss.sciencedaily.com/top.xml",
-    "EurekAlert! - Science": "https://www.eurekalert.org/rss/chemistry_physics.xml"
+    "Science Daily - Top": "https://rss.sciencedaily.com/top.xml"
 }
