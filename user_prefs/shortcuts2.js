e86307e9317216634abe88937e9c01c0d2817d3c
Prasanna Venkatesh <vipranarayan14@gmail.com>
2017-03-25 17:40:30 +0530

    move rssSources and create shortcuts2.js

---
+++ user_prefs/shortcuts2.js
@@ -0,0 +1,17 @@
+//Add shortcut URLs here.
+var shortcuts = {
+    "shortcuts": [
+        {
+            "shortcutTitle": "Apps",
+            "shortcutUrl": "chrome://apps"
+        },
+        {
+            "shortcutTitle": "Exts",
+            "shortcutUrl": "chrome://extensions"
+        },
+        {
+            "shortcutTitle": "Drive",
+            "shortcutUrl": "https://drive.google.com"
+        }
+    ]
+};
