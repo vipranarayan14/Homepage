d94fc0e3dd7088ab058c80d20c86cbc50bf585b8
Prasanna Venkatesh <vipranarayan14@gmail.com>
2017-04-24 21:14:39 +0530

    use only a `textarea` for testing. instead of `inputs`

--- scripts/options.js
+++ scripts/options.js
@@ -1,9 +1,15 @@
-var optionsModalContainer = document.querySelector('.options-modal-container');
+const optionsModalContainer = document.querySelector('.options-modal-container');
+const rssSourcesTitleInput = document.querySelector('.rssSources-title-input');
 
 document.querySelector('.options-modal-close-btn').addEventListener('click', () => {
 
     optionsModalContainer.style.display = 'none';
 });
 
-w3.displayObject("rssSources-list", rssSources);
-w3.displayObject("shortcuts-list", shortcuts);
+rssSourcesTitleInput.addEventListener('keyup', function (e) {
+    (e.keyCode === 13) ? localStorage.setItem("feedSources",JSON.stringify(JSON.parse(this.value))) : "";
+});
+
+// rssSourcesTitleInput.value = JSON.stringify(Rss.config.rssSources());
+// w3.displayObject("rssSources-list", rssSources);
+// w3.displayObject("shortcuts-list", shortcuts);
