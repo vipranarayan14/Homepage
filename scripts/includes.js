fbc654169b96435266da3247f4c70ab44da41f30
Prasanna Venkatesh <vipranarayan14@gmail.com>
2017-04-24 21:31:14 +0530

    remove `rss-sources.js`

--- scripts/includes.js
+++ scripts/includes.js
@@ -1,18 +1,23 @@
 // included from ../scripts/progress.js
 // @depends ../scripts/progress.js
+
 // included from ../user_prefs/shortcuts.js
 // @depends ../user_prefs/shortcuts.js
-// included from ../user_prefs/rss-sources.js
-// @depends ../user_prefs/rss-sources.js
+
 // included from ../assets/w3/w3.js
 // @depends ../assets/w3/w3.js
+
 // included from ../assets/rsser/rsser.js
 // @depends ../assets/rsser/rsser.js
+
 // included from ../assets/rsser/date-formatter.js
 // @depends ../assets/rsser/date-formatter.js
+
 // included from ../scripts/home.js
 // @depends ../scripts/home.js
+
 // included from ../scripts/options.js
 // @depends ../scripts/options.js
+
 // included from ../scripts/index.js
 // @depends ../scripts/index.js
